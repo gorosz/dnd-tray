5051ef10db2516aac0885b56dde03b9d52edb850
Gergely Orosz <gergely.orosz@balabit.com>
2020-06-24 16:30:08 +0200

    add new dependencies for publishing
    Signed-off-by: Gergely Orosz <gergelyo@protonmail.com>

--- tasks.py
+++ tasks.py
@@ -7,8 +7,21 @@ from invoke import task
 def clean(c):
     c.run("rm -rf build")
     c.run("rm -rf dnd_tray.egg-info")
+    c.run("rm -rf dist")
 
 
 @task(clean)
 def build(c):
     c.run("python setup.py build")
+
+
+@task(clean)
+def build_wheel(c):
+    c.run("python setup.py bdist_wheel")
+
+
+@task(build_wheel)
+def publish(c, test_release=True):
+    if test_release:
+        c.run("twine upload -r testpypi dist/*")
+    c.run("twine upload dist/*")
